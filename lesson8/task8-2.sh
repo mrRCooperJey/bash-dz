747348d81981c851a8d21649ae915df4a97b346e
mrRCooperJey <mrRCooperJey@yandex.ru>
2025-03-24 14:57:39 +0300

    Files for homework 8 #2

--- lesson8/task8-2.sh
+++ lesson8/task8-2.sh
@@ -3,7 +3,7 @@ days_of_week=("Monday" "Tuesday" "Wednesday" "Thursday" "Friday" "Saturday" "Sun
 read -p "Enter your number: " num
 
 if [[ $num -lt 0 || $num -gt 7  ]]; then
-	echo "eblan vel hyety"
+	echo "Please enter a number between 1 and 7"
 fi
 
-echo "${days_of_week[num - 1]}"
+echo "${days_of_week[num-1]}"
